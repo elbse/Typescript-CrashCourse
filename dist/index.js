ce902a982801f4072fb4e2c8cdd476e900223746
elbse <changpriego@gmail.com>
2025-10-12 20:02:03 +0800

    tsconfig

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 30;
+let firstName = 'Mario';
+let isFictional;
+console.log(age);
+firstName = 'Leonard';
+isFictional = true;
+let plane = 'Earth';
+let moons = 1;
+let isLarge = false;
+//# sourceMappingURL=index.js.map
