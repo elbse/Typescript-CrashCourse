edb6ea6cf70ddc2ede7ff9953d346e080cc8b5b2
elbse <changpriego@gmail.com>
2025-10-12 19:47:23 +0800

    TypeScript - Basics

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
-let age = 30;
+let age:number = 30;
+let firstName:string = 'Mario'
+let isFictional:boolean
 
-console.log(age)
+console.log(age);
+
+firstName = 'Leonard'
+
+isFictional = true
+
+let plane = 'Earth'
+let moons = 1
+let isLarge = false
