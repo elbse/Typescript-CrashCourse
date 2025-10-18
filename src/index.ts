625426862378a99e5ea2cc1bfaa4ba4848ddddc2
elbse <changpriego@gmail.com>
2025-10-18 22:02:27 +0800

    learning arrays

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,35 @@ isFictional = true
 let plane = 'Earth'
 let moons = 1
 let isLarge = false
+
+//arrays 
+
+let names: string[] = ['Sarah', 'Jerlyn', 'Peach']
+let years: number[] = [1990, 1918, 2000]
+
+names.push('Keo')
+years.push(2020)
+
+// type inference with arrays
+
+let fruits = ['Apple', 'Banana', 'Orange']
+
+// object literals 
+
+let car: { make: string, model: string, year: number} = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2020
+}
+car.year = 2021
+
+// type inference with object literals
+
+let laptop = {
+    brand: 'Dell',
+    ram: 16,
+    isTouchscreen: false
+}
+laptop.ram = 32
+laptop.isTouchscreen = true
+// laptop.brand = 123 // Error: Type 'number' is not assignable to type 'string'
